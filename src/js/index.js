6f0a07cc39b313699b793f4d1029bfc830718b2e
runningLunar <LunarLGJ@163.com>
2017-08-16 00:31:13 +0800

    first

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+$('#footer-nav-open').click(function () {
+    $('.foot-nav').css('display','block');
+});
+$('#footer-nav-close').click(function () {
+    $('.foot-nav').css('display','none');
+});
